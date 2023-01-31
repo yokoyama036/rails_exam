a66c64bbba629a2f6143b9514d5a2b1ae65d66f4
yokoyama <yokoyama@DESKTOP-5P6MFQI.localdomain>
2023-01-31 13:24:14 +0900

    error modified

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,62 +1,59 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update, :destroy]
   
-    def index
-      @posts = Post.all
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
-      @post = Post.new(post_params)
-      if params[:back]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "投稿しました！"
+      else
         render :new
-      else
-        if @post.save
-          redirect_to posts_path, notice: "投稿しました！"
-        else
-          render :new
-        end
       end
     end
-  
-    def show
-      @post = Post.find(params[:id])  
-    end
-  
-    def edit
-      @post = Post.find(params[:id])
-    end
-  
-    def update
-      @post = Post.find(params[:id])
-      if @post.update(blog_params)
-        redirect_to posts_path, notice: "編集しました！"
-      else
-        render :edit
-        render :new if @post.invalid?
-      end
-    end
-  
-    def confirm
-      @post = Post.new(post_params)
+  end
+
+  def show
+    @post = Post.find(params[:id])  
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "編集しました！"
+    else
+      render :edit
       render :new if @post.invalid?
     end
-  
-    def destroy
-      @post.destroy
-      redirect_to posts_path, notice:"削除しました！"
-    end
-    private
-  
-    def post_params
-      params.require(:posts).permit(:title, :content)
-    end
-  
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
-  
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice:"削除しました！"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
